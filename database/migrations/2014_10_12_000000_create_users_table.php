c5eb7f33c02312ff7142289d8fadf9ae9e9ed443
Hypes-Astro <muhamadalifanwar@gmail.com>
2025-05-31 14:31:16 +0700

    [fixing bug] sign up in production

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'konsumen']);
             $table->rememberToken();
             $table->timestamps();
         });
+        
     }
 
     /**
